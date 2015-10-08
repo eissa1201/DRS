6e4fe6766cf45464a489d7f30e3666353cba52b7
Ahmed Al-Eissa <Eissa1201@hotmail.com>
2015-10-08 17:11:58 +0300

    DRS v 0.4
    changed results listview to an imagecell

---
+++ DRS/DRS/DoctorSearch.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DRS
+{
+    public partial class DoctorSearch : ContentPage
+    {
+        public DoctorSearch()
+        {
+            InitializeComponent();
+            DoctorLogic obj = new DoctorLogic();
+            obj.Cell1 = "Dr.Ahmed";
+            obj.Cell2 = "Dr.Faisal";
+            obj.Cell3 = "Dr.sultan";
+            obj.Cell4 = "Dr.Mubark";
+            obj.Cell5 = "Dr.Saud";
+            obj.Cell6 = "Dr.muhammad";
+            obj.Cell7 = "Dr.mansoor";
+            this.BindingContext = obj;
+        }
+        void Next(object sender, EventArgs args)
+        {
+
+            Navigation.PushAsync(new WelcomePage());
+        }
+
+      
+    }
+}
